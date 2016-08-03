070b174a8e52946d5ddad1c29dccb9f81a6128ca
Student <student@Admins-Mac-mini-2.local>
2016-08-03 17:27:43 -0600

    Making sure it's all updated

--- node-colors/colorsApi/public/js/app.controller.home.js
+++ node-colors/colorsApi/public/js/app.controller.home.js
@@ -12,36 +12,44 @@ function homeController ($http) {
 
 	hCtrl.welcomeMessage = 'The only place for delicious nougat.';
 
+	//function that retrieves our information from the server
 	hCtrl.getTheColors = function (){
 
 		$http.get('/api/colors')
 			.then(function(response){
-				 console.log(response);
+				 // console.log(response);
+				 //this forEach loop iterates over all indexes of our response data, pushing each object into our hCtrl.colorInfo array
 				 response.data.forEach (function(item){
 				 	hCtrl.colorInfo.push(item);
 				 })
-				 console.log(hCtrl.colorInfo);
+				 // console.log(hCtrl.colorInfo);
 			});
 
 		console.log('Such colors! Much wow!');
 
 	}
 
+	//call the damn function (REMEMBER TO CALL FUNCTIONS)
 	hCtrl.getTheColors();
 
 	hCtrl.userColor = '';
 
-	hCtrl.matchTheValues = function() {
-		// console.log(hCtrl.userColor);		
+	hCtrl.matchTheValues = function() {		
 		var color = hCtrl.userColor.toLowerCase();
 		var hex = hCtrl.hexValues;
 		var rgb = hCtrl.rgbValues;
 
-		console.log(color,hex,rgb);
+		// console.log(color,hex,rgb);
 
-	// 	if (color == )
-
-	// }
+		// iterates over our hCtrl.colorInfo array, checking to see if the value matches the hCtrl.userColor input from main.html
+		hCtrl.colorInfo.forEach (function(item){
+			if (color == item.name.toLowerCase()){
+				console.log('Success!');
+			}
+			else {
+				console.log('Frowny face');
+			}
+		})
 
 }
 };
--- node-colors/colorsApi/server.js
+++ node-colors/colorsApi/server.js
@@ -20,15 +20,19 @@ app.get('/', function(req, res){
 	});
 });
 
+// Here we are pulling in the color data from the JSON file
 app.get('/api/colors', function(req, res){
-	var colorData = [];
 	//1 - use REQUEST to grab Color info
 	request('https://cdn.rawgit.com/metaraine/swatch/74580660c9229541622bbf1fd4198618d9f677e5/webcolors.json'
-	, 	function(err, response, body){
+	, 	function(err, response, body){  
 			// console.log(response.body);
 			// console.log(typeof response.body)
+
+			//data is the body, which ignores the status, statusText, config, etc.
 			var data = response.body;
+			//json parses this string response into an object, collection of arrays
 			var json = JSON.parse(data);
+			// we send the data at the end in its new object form
 			res.send(json);
 		})
 	// res.send(colorData)
